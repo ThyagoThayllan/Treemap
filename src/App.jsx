19e031dcf4eb99fba720cd45c5588f6d6ae18c5f
Thyago Thayllan <thayllanst@outlook.com>
2023-11-15 19:37:24 -0300

    commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { Treemap, ResponsiveContainer, Tooltip } from "recharts";
+import minhaFoto from './imgs/minhaFoto.jpg'
+import styles from './App.module.css';
 
 const renderTooltipContent = (props) => {
   const { payload } = props;
 
   if (payload && payload.length > 0) {
-    const { name, value } = payload[0].payload;
+    const { value } = payload[0].payload;
     return (
-      <div style={{backgroundColor: '#333', borderRadius: '10px', padding: '1rem'}}>
-        <p><strong>Valor Total:</strong> R$ {value}</p>
+      <div
+        style={{
+          backgroundColor: "#333",
+          borderRadius: "10px",
+          padding: "1rem",
+        }}
+      >
+        <p>
+          <strong>Valor Total:</strong> R$ {value}
+        </p>
       </div>
     );
   }
@@ -26,14 +36,14 @@ const TreeMapChart = ({ data }) => {
   };
 
   return (
-    <div style={{ width: '50%', height: '400px' }}>
+    <div style={{ width: "50%", height: "400px" }}>
       <ResponsiveContainer>
         <Treemap
           data={treemapData.children}
           dataKey="value"
           ratio={4 / 3}
-          stroke="#000"
-          fill="#8884d8"
+          stroke="#fff"
+          fill="#008000"
         >
           <Tooltip content={renderTooltipContent} />
         </Treemap>
@@ -86,10 +96,71 @@ export const App = () => {
     },
   ];
 
+  const totalProductsSold = products
+    .reduce((acc, product) => acc + product.price * product.amount, 0)
+    .toLocaleString("pt-BR", { style: "currency", currency: "BRL" });
+
+  const date = new Date()
+  const formattedDate = new Intl.DateTimeFormat('pt-BR').format();
+
   return (
-    <div style={{display: 'flex', alignItems: 'center', justifyContent: 'center', flexFlow: 'column', height: '100vh'}}>
-      <h1>Treemap de Produtos</h1>
+    <div
+      style={{
+        display: "flex",
+        alignItems: "center",
+        justifyContent: "center",
+        flexFlow: "column",
+        minHeight: "100vh",
+        position: 'relative',
+        overflow: 'hidden',
+        padding: '2rem'
+      }}
+    >
+      <div style={{display: 'flex', flexFlow: 'column', alignItems: 'center', marginBottom: "10px"}}>
+        <h1 style={{ textTransform: 'uppercase' }}>Vendas de hoje</h1>
+        <p style={{fontStyle: 'italic', textDecoration: 'underline'}}>{formattedDate}</p>
+      </div>
       <TreeMapChart data={products} />
+      <p style={{ margin: "10px" }}><b>Total de vendas hoje:</b></p>
+      <h2 style={{padding: '1rem 2rem', marginBottom: '2rem', backgroundColor: '#0f6a08', borderRadius: '10px'}}>{totalProductsSold}</h2>
+      <div>
+        {products.map((product) => (
+          <div key={product.name} className={styles.productCard}>
+            <p className={styles.product}><b>Produto:</b> {product.name}</p>
+            <p className={styles.product}><b>Preço:</b> {(product.price).toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})}</p>
+            <p className={styles.product}><b>Quantidade:</b> {product.amount}</p>
+          </div>
+        ))}
+      </div>
+
+      <div className={styles.profile}>
+        <div className={styles.iconeMenu}>☰</div>
+        <p>Feito for <a href='https://www.linkedin.com/in/thyago-thayllan-mendes-de-sousa-2058b0239/' target='_blank'>Thyago Thayllan</a></p>
+        <img src={minhaFoto} alt="Foto de Thyago Thayllan" />
+        <div className={styles.info}>
+          <p className={styles.name}>Thyago Thayllan</p>
+          <p className={styles.job}>Desenvolvedor Front-End</p>
+        </div>
+        <div className={styles.technologies}>
+          <h2>Tecnologias</h2>
+          <div>
+           <ul>
+              <li>ReactJS</li>
+              <li>JavaScript</li>
+              <li>TypeScript</li>
+            </ul>
+            <ul>
+              <li>React Native</li>
+              <li>HTML</li>
+              <li>CSS</li>
+            </ul>
+          </div>
+        </div>
+        <div className={styles.buttons}>
+          <a target='_blank' className={styles.linkedin} href="https://www.linkedin.com/in/thyago-thayllan-mendes-de-sousa-2058b0239/">LinkedIn</a>
+          <a target='_blank' className={styles.github} href="https://github.com/ThyagoThayllan">GitHub</a>
+        </div>
+      </div>
     </div>
   );
 };
